d5a4336a6806af881d8d6332377e139e10b0fda7
natural-mess <37369211+natural-mess@users.noreply.github.com>
2026-01-16 14:50:14 +0100

    Finish ship, alien, game settings

--- settings.py
+++ settings.py
@@ -12,11 +12,22 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 2.5
         self.bullet_width = 3
+        # Use this bullet_width to make bigger bullet for testing
+        # self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # For testing purpose, increase drop speed of fleet
+        # self.fleet_drop_speed = 100
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
     
